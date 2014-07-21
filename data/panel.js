a4e2e3b225c0a4e97f19cde2397f00f59a9db0f1
sala <saladinkzn@gmail.com>
2014-07-21 23:53:34 +0400

    startLiking, stopLiking toggling

--- data/panel.js
+++ data/panel.js
@@ -1,16 +1,21 @@
-﻿var intervalId;
-var likeButton = document.getElementById('startLiking');
+﻿var likeButton = document.getElementById('startLiking');
+var stopLikeButton = document.getElementById('stopLiking');
+//
 if(likeButton) {
 	likeButton.onclick = function() {
 		self.port.emit('startLike');
 		// TODO: enable disable button
 		// TODO: disable self
+		likeButton.disabled = true;
+		stopLikeButton.disabled = false;
 	}
 }
-var stopLikeButton = document.getElementById('stopLiking');
+//
 if(stopLikeButton) {
 	stopLikeButton.onclick = function() {
 		self.port.emit('stopLike');
+		likeButton.disabled = false;
+		stopLikeButton.disabled = true;
 	}
 }
 
