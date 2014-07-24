8f4f8952cb9f2d81be869cb191357e8d6b5af602
sala <saladinkzn@gmail.com>
2014-07-25 02:02:07 +0400

    panel button was removed

--- data/my-script.js
+++ data/my-script.js
@@ -1,8 +1,31 @@
-function like() {
-	console.log('like');
-	var likeButton = document.querySelector('.dating-button-sympathy');
-	likeButton.click();
+var likeButton = document.querySelector('.dating-button-sympathy');
+if(!likeButton) {
+	console.log('likeButton was not found');
+} else {
+	var likeButtonParent = likeButton.parentElement;
+	//
+	var startLikingLabel = 'Начать лайкать';
+	var stopLikingLabel = 'Закончить лайкать'
+	var roboButton = document.createElement('a');
+	roboButton.className = 'standard_star blue-button dating-button-common';
+	roboButton.innerHTML = startLikingLabel;
+	roboButton.style = 'padding-left: 10px;';
+	likeButtonParent.insertBefore(roboButton, likeButton);
+	//
+	var intervalId = null;
+	roboButton.onclick = function() {
+		if(!intervalId) {
+			intervalId = setInterval(function() {
+				likeButton.click();
+			}, 500);
+			roboButton.innerHTML = stopLikingLabel;
+		} else {
+			clearInterval(intervalId);
+			intervalId = null;
+			roboButton.innerHTML = startLikingLabel;			
+		}
+	}
 }
 
-self.port.on('like', like);
+
 
